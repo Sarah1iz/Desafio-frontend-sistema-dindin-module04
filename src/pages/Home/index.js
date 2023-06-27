3dd720f79dc7f9adf33c96c95f1fd6ec732402bb
Alisson Rodrigo <alisson.rodrigo1307@gmail.com>
2023-06-27 18:58:14 -0300

    estrutura tabela ok

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,19 +2,42 @@ import './style.css';
 import logo from '../../assets/logo.svg';
 import avatar from '../../assets/avatar.svg';
 import sair from '../../assets/logout.svg';
+import filtrar from '../../assets/icon_filter.svg';
 
 function Home() {
     return (
         <>
-            <header className='header'>
+            <header>
                 <img className='logo' src={logo} alt='logo' />
 
-                <div className='container__menu'>
+                <div className='container_menu'>
                     <img src={avatar} alt='logo' />
                     <h3 className='menu_nome'>Usuário</h3>
-                    <img src={sair} alt='logo' />
+                    <img src={sair} alt='logout' />
                 </div>
             </header>
+
+            <main className='container_principal'>
+                <button className='btn_filtro'>
+                    <img src={filtrar} alt='filtrar' />
+                    Filtrar
+                </button>
+
+                <div className='container_painel'>
+                    <div className='tabela_cabecalho'>
+                        <p className='item_data'>Data</p>
+                        <p className='item_dia'>Dia da Semana</p>
+                        <p className='item_descricao'>Descrição</p>
+                        <p className='item_categoria'>Categoria</p>
+                        <p className='item_valor'>Valor</p>
+                        <span className='icons'></span>
+                    </div>
+
+
+
+                    {/* resumo */}
+                </div>
+            </main>
         </>
     );
 }
