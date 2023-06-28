65f0efaecfb1515a39a804b60b69f726b1638dd5
Alisson Rodrigo <alisson.rodrigo1307@gmail.com>
2023-06-28 19:40:04 -0300

    ajustando resumo com atributos de estados

--- src/components/Resumo/index.js
+++ src/components/Resumo/index.js
@@ -1,8 +1,7 @@
-//import ModalRegistro from "../Modal/ModalRegistro";
-import './style.css';
-//import { useState } from 'react';
 
-function Resumo() {
+import './style.css';
+
+function Resumo({ stateModal, valorResumo }) {
 
 
     return (
@@ -13,23 +12,24 @@ function Resumo() {
                 <div className='container_transacoes'>
                     <div className='entradas'>
                         <p className='transacoes_titulo'>Entradas</p>
-                        <p className='transacoes_entradas'>R$<span>valor</span></p>
+                        <p className='transacoes_entradas'>R${valorResumo.entrada}</p>
                     </div>
 
                     <div className='saidas'>
                         <p className='transacoes_titulo'>Saídas</p>
-                        <p className='transacoes_saidas'>R$<span>valor</span></p>
+                        <p className='transacoes_saidas'>R${valorResumo.saida}</p>
                     </div>
 
                     <div className='resumo_saldo'>
                         <h3 className='saldo_titulo'>Saldo</h3>
-                        <p className='saldo'>R$<span>valor</span></p>
+                        <p className='saldo'>R${valorResumo.saldo}</p>
                     </div>
                 </div>
             </div>
 
             <button
                 className="btn-purple btn-registro"
+                onClick={stateModal}
             >
                 Adicionar Registro
             </button>
