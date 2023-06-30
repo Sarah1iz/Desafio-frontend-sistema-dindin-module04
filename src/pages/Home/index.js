ac64a843fb9bccdc64a0bb580be372528ba18548
Alisson Rodrigo <alisson.rodrigo1307@gmail.com>
2023-06-30 13:51:20 -0300

    fix home e arrumando componentes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,34 +1,91 @@
-import { useState } from "react";
 import './style.css';
 import Filtro from '../../components/Filtro';
-import CabecalhoTabela from '../../components/CabecalhoTabela';
+import Tabela from '../../components/Tabela';
 import Resumo from '../../components/Resumo';
-//import Cabecalho from '../../components/Cabecalho';
+import Cabecalho from "../../components/Cabecalho";
+import Modal from "../../components/Modal";
 
+import { format } from "date-fns";
+import { useEffect, useState } from "react";
+import { getRegistro } from "../../utils/functionsDash";
 
-//import { getTransacao } from '../../utils/functionsDash';
 
 function Home() {
-    const [modalState, setModalState] = useState(false)
+    const [registro, setRegistro] = useState([]);
+    const [modalState, setModalState] = useState(false);
+    const [current, setCurrent] = useState(null);
+    const [corButton, setCorButton] = useState(false);
 
-    const [valorResumo, setValorResumo] = useState({
+    const [form, setForm] = useState({
+        tipo: "entrada",
+        valor: "",
+        categoria_id: "",
+        data: "",
+        descricao: "",
+    });
+
+
+    const [valorResumo, setRegistroResumo] = useState({
         entrada: 0,
         saida: 0,
         saldo: 0,
     });
 
-    function handleModal(transacoes) {
+    useEffect(() => {
+        getRegistro(setRegistro, setRegistroResumo);
+
+    }, []);
+
+    function handleModal(registro) {
+        if (!modalState) {
+            setModalState(true);
+            if (registro.tipo === "entrada") {
+                setCorButton(true);
+            }
+            if (registro.id) {
+                const data = format(new Date(registro.data).setHours(+24), "yyyy-MM-dd");
+                const valor = registro.valor;
+                setForm({ ...registro, data, valor });
+                setCurrent(registro.id);
+
+                return;
+            }
+            setCurrent(null);
+            setForm({
+                tipo: "saida",
+                descricao: "",
+                valor: "",
+                data: "",
+                categoria_id: "",
+            });
+            return;
+        }
         setModalState(false);
+        setCorButton(false);
     }
 
     return (
         <>
-            {/* <Cabecalho /> */}
+            <Cabecalho />
+
+            <Modal
+                state={modalState}
+                handleModal={handleModal}
+                form={form}
+                setForm={setForm}
+                setRegistro={setRegistro}
+                current={current}
+                setRegistroResumo={setRegistroResumo}
+                setCorButton={setCorButton}
+                corButton={corButton}
+            />
 
             <main className='container_principal'>
                 <Filtro />
                 <div className='dashboard'>
-                    <CabecalhoTabela />
+                    <Tabela
+                        registro={registro}
+                    />
                     <Resumo
                         stateModal={handleModal}
                         valorResumo={valorResumo}
