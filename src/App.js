7b9939f3b92e783267e13357125a630ec0ff4ca4
Alisson Rodrigo <alisson.rodrigo1307@gmail.com>
2023-06-30 13:43:24 -0300

    criando rotas protegidas

--- src/App.js
+++ src/App.js
@@ -2,20 +2,38 @@ import Cadastrar from "./pages/Cadastrar";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import { Outlet, Route, Routes, Navigate } from "react-router-dom";
+let token = localStorage.getItem("token");
 
 
 
 function App() {
 
-  //criar rotas protegidas
+  function ProtectedHome({ redirectTo }) {
+    token = localStorage.getItem("token");
+    return token ? <Outlet /> : <Navigate to={redirectTo} />;
+  }
+
+  function ProtectedLogin({ redirectTo }) {
+    token = localStorage.getItem("token");
+    return !token ? <Outlet /> : <Navigate to={redirectTo} />;
+  }
 
 
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/cadastro" element={<Cadastrar />} />
-      <Route path="/home" element={<Home />} />
+      <Route element={<ProtectedHome redirectTo="/" />}>
+        <Route path="/home" element={<Home />} />
+      </Route>
+
+      <Route element={<ProtectedLogin redirectTo="/home" />}>
+
+        <Route path="/cadastro" element={<Cadastrar />} />
+        <Route path="">
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+
+        </Route>
+      </Route>
     </Routes>
   );
 }
